31bc2d8908947c484303b9043084b3695503e09a
Harold06Hoyos <harold.hoyos@ipsumtechnology.co>
2024-08-23 00:18:55 -0500

    Consultas para validacion de ingreso de usuario al dashboard

---
+++ Model/val_login.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+class logUser
+{
+
+    public function existUser($email, $conexion)
+    {
+        $sql = $conexion->query("SELECT * FROM usuarios WHERE email = '$email'");
+
+        if ($sql->num_rows < 0) {
+            echo "
+                    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+                    <script language='JavaScript'>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Este usuario no esta registrado',
+                            showCancelButton: false,
+                            confirmButtonColor: '#FF0000',
+                            confirmButtonText: 'OK',
+                            timer: 6000
+                        }).then(() => {
+                            location.assign('../view/register.php');
+                        });
+                    });
+                    </script>";
+            exit();
+        }
+    }
+
+
+    public function loginUser($email, $contrasena, $conexion)
+    {
+        $sql = $conexion->query("SELECT * FROM usuarios WHERE email = '$email'");
+
+        try {
+            if ($sql->num_rows > 0) {
+                $datos = $sql->fetch_assoc();
+                $contrasenaHash = $datos["contrasena"];
+                $email = $datos["email"];
+                if (password_verify($contrasena, $contrasenaHash)) {
+                    $_SESSION["email"] = $email;
+                    echo '<script>window.location.href="../view/dashboard.php";</script>';
+                } else {
+                    echo "
+                    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+                    <script language='JavaScript'>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error al comprar contraseña',
+                            showCancelButton: false,
+                            confirmButtonColor: '#FF0000',
+                            confirmButtonText: 'OK',
+                            timer: 6000
+                        }).then(() => {
+                            location.assign('../View/user_login.php');
+                        });
+                    });
+                    </script>";
+                    exit();
+                }
+            } else {
+                echo "
+                    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+                    <script language='JavaScript'>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error al realizar la consulta',
+                            showCancelButton: false,
+                            confirmButtonColor: '#FF0000',
+                            confirmButtonText: 'OK',
+                            timer: 6000
+                        }).then(() => {
+                            location.assign('../View/user_login.php');
+                        });
+                    });
+                    </script>";
+
+                exit();
+            }
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}

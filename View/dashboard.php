83a2e2d27294e55f54dd843c3b4c129ebb01aeb3
Harold06Hoyos <harold.hoyos@ipsumtechnology.co>
2024-08-24 15:45:08 -0500

    Actualiazion de la informacion de usuario

--- View/dashboard.php
+++ View/dashboard.php
@@ -1,15 +1,45 @@
+<?php
+session_start();
+if (!isset($_SESSION["userId"])) {
+    echo '<script>window.location.href="../view/user_login.php";</script>';
+}
+
+require_once '../model/consultar_users.php';
+$id = $_SESSION["userId"];
+$classUser = new User();
+$users = $classUser->consultar($id);
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Tarjeta de Usuario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <h2>Welcome,</h2>
-    <a href="/logout">Logout</a>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="user-card">
+
+            <div class="user-info">
+                <?php
+                $consulta_usuarios = include("../model/consultar_users.php");
+
+                ?>
+            </div>
+            <div class="user-photo">
+                <img src="
+              css/imagenes/Header.png
+                " alt="Foto de Usuario">
+                <?php
+                ?>
+
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
